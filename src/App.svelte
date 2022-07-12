12bb1def234c1a0b07191665db2f13a5fd808de4
John Gibson <jg20019@gmail.com>
2022-07-12 19:39:36 -0400

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,69 @@
+<script>
+import Card from './Card.svelte'
+
+let cards = [
+    { front: "+",  back:"Add",       showFront:true },
+    { front: "-",  back:"Subtract",  showFront:true },
+    { front: "*",  back:"Multiply",  showFront:true },
+    { front: "/",  back:"Divide",    showFront:true },
+    { front: "%",  back:"Modulus",   showFront:true },
+    { front: "++", back:"Increment", showFront:true },
+    { front: "--", back:"Decrement", showFront:true },
+]
+
+let index = 0
+
+$: card = cards[index]
+$: length = cards.length
+
+function prevCard() {
+    index = Math.max(0, (index - 1))
+}
+
+function flipCard() {
+    console.log('flipping card')
+    cards[index].showFront = !cards[index].showFront
+}
+
+function nextCard() {
+    index = (index + 1) % length
+}
+
+</script>
+
+<main> 
+<Card 
+    front="{card.front}"
+    back="{card.back}" 
+    showFront="{card.showFront}"
+/>
+<div class="controls">
+    <button on:click={prevCard}> Prev </button>
+    <button on:click={flipCard}> Flip </button>
+    <button on:click={nextCard}> Next </button>
+</div>
+</main>
+
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  main {
+    width: max-content;
+    margin: 0 auto;
+  }
+
+  .controls {
+    margin-top: 10px;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .controls button {
+    padding: 5px 10px;
+    font-family: monospace;
+  }
+</style>
---
+++ src/Card.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let front
+    export let back
+
+    export let showFront = true
+
+    $: cardText = showFront ? front : back
+
+</script>
+
+<div class="card">
+    <span> { cardText } </span>
+</div>
+
+<style>
+    .card {
+        display: flex;
+        max-width: 480px;
+        min-width: 320px;
+        height: 240px;
+        display: flex;
+        font-size: 48px;
+        border: 2px solid black;
+        justify-content: center;
+        align-items: center;
+        background-color: white;
+    }
+</style>
